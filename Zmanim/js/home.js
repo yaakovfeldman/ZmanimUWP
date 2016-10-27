7b3d3de2346b63d6a41972078d2193dd04f3813e
Yaakov Feldman <jfield123s@gmail.com>
2016-10-27 23:00:06 +0100

    added default page

---
+++ Zmanim/js/home.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function() {
+    var hebDate = new Hebcal.HDate();
+    dateDisplay.innerText = hebDate.toString();
+    var zmanim = hebDate.getZemanim();
+    chatzotDisplay.innerText = "Chatzot: " + zmanim.chatzot.getHours() + ":" + zmanim.chatzot.getMinutes();
+
+    var zman
+    for (zman in zmanim) {
+        zmanimDisplay.innerHTML += "<br>" + zman + zmanim[zman].getHours() + ":" + zmanim[zman].getMinutes();
+    }
+})
--- Zmanim/js/main.js
+++ Zmanim/js/main.js
@@ -59,17 +59,11 @@ WinJS.Binding.processAll(null, Sample).then(function () {
 })
 
 
-	    var hebDate = new Hebcal.HDate();
-	    dateDisplay.innerText = hebDate.toString();
-	    var zmanim = hebDate.getZemanim();
-	    chatzotDisplay.innerText = "Chatzot: " + zmanim.chatzot.getHours() + ":" + zmanim.chatzot.getMinutes();
 
-        var zman
-        for (zman in zmanim) {
-            zmanimDisplay.innerHTML += "<br>" + zman + zmanim[zman].getHours() + ":" + zmanim[zman].getMinutes();
-        }
 
-	    Sample.updateSplitView('small');
+Sample.updateSplitView('small');
+
+WinJS.Navigation.navigate("/pages/home.html");
 
 
 
